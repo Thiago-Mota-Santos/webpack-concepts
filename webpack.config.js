e8e09f522a24a3a87fb6d0feaf197972b41385ec
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-02-07 10:29:27 -0300

    feat(webpack) att

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,21 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        index: './src/index.ts',
-        print: './src/print.ts'
-    },  
-    devtool: 'inline-source-map',
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-          {
-            test: /\.ts?$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-          },
-          {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader'],
-          },
-          {
-            test: /\.(woff|woff2|eot|ttf|otf)$/i,
-            type: 'asset/resource',
-          },
-        ],      
+  entry: './src/index.ts',  
+  output: {
+    filename: 'bundle.js',  
+    path: path.resolve(__dirname, 'dist'),  
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],  
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,  
+        use: 'ts-loader', 
+        exclude: /node_modules/, 
       },
-      plugins: [
-        new HtmlWebpackPlugin({
-          title: "project",
-          template: "index.html"
-        })
-      ],
-      resolve: {
-        extensions: ['.ts', 'js']
-      },
-}
+    ],
+  },
+};
